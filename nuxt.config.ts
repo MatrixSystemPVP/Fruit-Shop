b14c6bea371c2f8b332d7017704e8b207581951f
MatrixSystemPVP <kivikplay2003@gmail.com>
2024-12-15 18:24:33 +0100

    Update Nuxt Config (Cloudflare Pages Deployment)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,11 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@nuxt/ui', '@nuxt/image'],
   compatibilityDate: '2024-12-06',
-  ssr: false
+  ssr: false,
+  nitro: {
+    preset: 'cloudflare-pages-static',
+    prerender: {
+      autoSubfolderIndex: false
+    }
+  }
 })
