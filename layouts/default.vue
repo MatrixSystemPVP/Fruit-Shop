e347c9153ed042c6a0656df936e53be1cb95add8
MatrixSystemPVP <kivikplay2003@gmail.com>
2024-12-15 14:52:35 +0100

    Create Layout and Page Structure

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <UHorizontalNavigation :links class="mb-8" />
+    <slot />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import type { HorizontalNavigationLink } from '#ui/types'
+
+const links: HorizontalNavigationLink[] = [
+  { label: 'Home', icon: 'i-heroicons-home', to: '/' },
+  { label: 'Products', icon: 'i-heroicons-building-storefront', to: '/products' },
+  { label: 'Customers', icon: 'i-heroicons-user-group-solid', to: '/customers' },
+  { label: 'Orders', icon: 'i-heroicons-clipboard-document-list', to: '/orders' }
+]
+</script>
