9edcffe90968055554dce3a6d02f2ba3299d4d79
MatrixSystemPVP <kivikplay2003@gmail.com>
2025-01-10 14:43:48 +0100

    Added Cart to Navigation

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,11 +7,17 @@
 
 <script lang="ts" setup>
 import type { HorizontalNavigationLink } from '#ui/types'
+import type { Reactive } from 'vue'
 
-const links: HorizontalNavigationLink[] = [
-  { label: 'Home', icon: 'i-heroicons-home', to: '/' },
-  { label: 'Products', icon: 'i-heroicons-building-storefront', to: '/products' },
-  { label: 'Customers', icon: 'i-heroicons-user-group-solid', to: '/customers' },
-  { label: 'Orders', icon: 'i-heroicons-clipboard-document-list', to: '/orders' }
-]
+const { itemCount } = useCart()
+
+const links: Reactive<HorizontalNavigationLink[][]> = reactive([
+  [
+    { label: 'Home', icon: 'i-heroicons-home', to: '/' },
+    { label: 'Products', icon: 'i-heroicons-building-storefront', to: '/products' },
+    { label: 'Customers', icon: 'i-heroicons-user-group-solid', to: '/customers' },
+    { label: 'Orders', icon: 'i-heroicons-clipboard-document-list', to: '/orders' }
+  ],
+  [{ label: 'Cart', icon: 'i-heroicons-shopping-cart', to: '/cart', badge: itemCount }]
+])
 </script>
